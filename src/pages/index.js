6befb9471a793e55a84a4e0c72da025ee2bf25da
tommy3531 <thomas.marler@hotmail.com>
2019-01-23 20:32:21 -0600

    Initial Commit

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">HomePage</div>
+        <div className="col-6">Hello</div>
+      </div>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
